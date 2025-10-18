0094d8045cd6675129d0ea7b6d619f5fc4a7fcfe
cybenko.sergey <cybenko.sergey@gmail.com>
2025-10-18 11:46:41 -0600

    add error page

---
+++ controllers/mainController.js
@@ -0,0 +1,20 @@
+const classificationModel = require("../models/classificationModel")
+
+const mainController = {}
+
+mainController.error = async (req, res, next) => {
+    try {
+      const classifications = await classificationModel.getAll()
+      const title = `Error`
+  
+      return res.render("error", {
+        classifications,
+        title
+      })
+  
+    } catch (error) {
+      return next(error)
+    }
+}
+
+module.exports = mainController
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ const router = express.Router()
 const indexController = require("../controllers/indexController")
 const inventoryRoutes = require("./inventory")
 const classificationRoutes = require("./classification")
+const mainController = require("../controllers/mainController")
+
 
 router.get("/", indexController.buildHome)
+router.get("/error", mainController.error)
 router.use("/inventory-model", inventoryRoutes)
 router.use("/classifications", classificationRoutes)
 
